3979b705abcc50509393933e0e37d114911ef0e2
person-kuldeep <person.kuldeep@gmail.com>
2026-06-18 12:18:47 +0530

    C_05_web_scraping_top25_Rotton_Tommato_movies_of_2000s

--- m_01_concept_learning/web_scraping/web_scrap.py
+++ m_01_concept_learning/web_scraping/web_scrap.py
@@ -1,11 +1,6 @@
-# Scenario
-
-# Consider that you have been hired by a Multiplex management organization to extract the information of the top 50 movies with the best average rating from the web link shared below.
-
 # https://web.archive.org/web/20230902185655/https://en.everybodywiki.com/100_Most_Highly-Ranked_Films
 
-# The information required is Average Rank, Film, and Year.
-# You are required to write a Python script webscraping_movies.py that extracts the information and saves it to a CSV file top_50_films.csv. You are also required to save the same information to a database Movies.db under the table name Top_50.
+# The information required is Rotton Tommato Rank, Film, and Year. of 2000s "2000" included, and only top 25
 
 # Required Libs
 import pandas as pd
@@ -17,10 +12,10 @@ import sqlite3
 url = 'https://web.archive.org/web/20230902185655/https://en.everybodywiki.com/100_Most_Highly-Ranked_Films'
 
 db_name = "Movies.db"
-table_name = "Top_50"
+table_name = "Rotton_25"
 
-csv_path = "top_50_films.csv"
-df =  pd.DataFrame(columns=["Average Rank","Film","Year"])
+csv_path = "top_25_rotton_films.csv"
+df =  pd.DataFrame(columns=["Rotton Tommato","Film","Year"])
 count = 0
 
 # Get Html Data
@@ -32,25 +27,30 @@ tables = data.find_all('tbody')
 rows = tables[0].find_all('tr')
 
 for row in rows:
-    if count<=50:
         column = row.find_all('td')
         if len(column)!=0:
-            data_dict = {
-                # contents gives a list even if there only one element in it
-                "Average Rank": int(column[0].contents[0]), 
-                "Film": column[1].contents[0],
-                "Year": int(column[2].contents[0])
+            if column[2].contents[0] != 'unranked' and column[3].contents[0] != 'unranked' :
+                if int(column[2].contents[0]) <= 2000: 
+                    data_dict = {
+                        # contents gives a list even if there only one element in it
+                        "Rotton Tommato": int(column[3].contents[0]), 
+                        "Film": column[1].contents[0],
+                        "Year": int(column[2].contents[0])
+                    }
+                    df_instant = pd.DataFrame(data_dict, index= [0])
+                    # main dataframe concatination
+                    df = pd.concat([df,df_instant], ignore_index=True)
 
-            }
-            df_instant = pd.DataFrame(data_dict, index= [0])
-            
-            # main dataframe concatination
-            df = pd.concat([df,df_instant], ignore_index=True)
+                    count += 1
+                else:
+                    pass
+            else:
+                pass
 
-            count += 1
-    else:
-        break
+# Top 25 movies of 2000s from available Rotton Tommato Rank on the webpage , unranked are not included
 
+df = df.sort_values(by="Rotton Tommato",ignore_index=True)[:25]
+print(df)
 # Storing data in sqlite db
 
 df.to_csv(csv_path)
